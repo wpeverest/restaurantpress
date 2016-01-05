ee38ad3d2c0a08ca3720192afa28fdbd97e192ed
Shiva Poudel <dev.shivapoudel@gmail.com>
2016-01-05 14:39:46 +0545

    Version bumps

--- Gruntfile.js
+++ Gruntfile.js
@@ -29,7 +29,7 @@ module.exports = function( grunt ){
 		// Minify .js files.
 		uglify: {
 			options: {
-				preserveComments: 'some'
+				preserveComments: false
 			},
 			admin: {
 				files: [{
