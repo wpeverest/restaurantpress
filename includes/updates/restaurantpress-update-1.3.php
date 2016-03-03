470ccb54835c5bb2b64d2ce19ddffb7a68935296
Shiva Poudel <dev.shivapoudel@gmail.com>
2016-03-03 14:17:01 +0545

    Migrate term meta to WordPress tables
    Although previous version supports this but we need this for future so let's manage in this script while running updater for this version only ;)

--- includes/updates/restaurantpress-update-1.3.php
+++ includes/updates/restaurantpress-update-1.3.php
@@ -13,3 +13,12 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 global $wpdb;
+
+/**
+ * Migrate term meta to WordPress tables.
+ */
+if ( get_option( 'db_version' ) >= 34370 && $wpdb->get_var( "SHOW TABLES LIKE '{$wpdb->prefix}restaurantpress_termmeta';" ) ) {
+	if ( $wpdb->query( "INSERT INTO {$wpdb->termmeta} ( term_id, meta_key, meta_value ) SELECT restaurantpress_term_id, meta_key, meta_value FROM {$wpdb->prefix}restaurantpress_termmeta;" ) ) {
+		$wpdb->query( "DROP TABLE IF EXISTS {$wpdb->prefix}restaurantpress_termmeta" );
+	}
+}
