e25ed1a24d17cf7f3d820d131a88e6d03ef994ad
Shiva Poudel <dev.shivapoudel@gmail.com>
2016-03-07 16:52:33 +0545

    Upgrade food grouping postmeta term slug to ID

--- includes/updates/restaurantpress-update-1.3.php
+++ includes/updates/restaurantpress-update-1.3.php
@@ -22,3 +22,42 @@ if ( get_option( 'db_version' ) >= 34370 && $wpdb->get_var( "SHOW TABLES LIKE '{
 		$wpdb->query( "DROP TABLE IF EXISTS {$wpdb->prefix}restaurantpress_termmeta" );
 	}
 }
+
+/**
+ * Migrate food grouping meta to term ID instead of slug.
+ */
+function rp_upgrade_food_grouping_slug_to_id() {
+	$existing_food_groups = $wpdb->get_results( "SELECT * FROM {$wpdb->postmeta} WHERE meta_key = 'food_grouping' AND meta_value != '';" );
+
+	if ( $existing_food_groups ) {
+
+		foreach ( $existing_food_groups as $existing_food_group ) {
+
+			$needs_update = false;
+			$new_value    = array();
+			$value        = maybe_unserialize( trim( $existing_food_group->meta_value ) );
+
+			if ( $value ) {
+				foreach ( $value as $key => $food_data ) {
+					if ( empty( $food_data ) || is_array( $food_data ) ) {
+						continue;
+					}
+
+					if ( ! is_numeric( $food_data ) ) {
+						$needs_update      = true;
+						$food_menu_term    = get_term_by( 'slug', $food_data, 'food_menu_cat' );
+						$new_value[ $key ] = (int) $food_menu_term->term_id;
+					} else {
+						$new_value[ $key ] = $food_data;
+					}
+				}
+				if ( $needs_update ) {
+					$new_value = serialize( $new_value );
+
+					$wpdb->query( $wpdb->prepare( "UPDATE {$wpdb->postmeta} SET meta_key = %s, meta_value = %s WHERE meta_id = %d", 'food_grouping', $new_value, $existing_food_group->meta_id ) );
+				}
+			}
+		}
+	}
+}
+add_action( 'init', 'rp_upgrade_food_grouping_slug_to_id' );
