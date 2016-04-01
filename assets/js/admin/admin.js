9445c923174c19ec1c9393add7608f3ddb2685ea
Shiva Poudel <dev.shivapoudel@gmail.com>
2016-04-01 10:50:27 +0545

    Tweak init tooltips

--- assets/js/admin/admin.js
+++ assets/js/admin/admin.js
@@ -4,12 +4,19 @@
 jQuery( function ( $ ) {
 
 	// Tooltips
-	var tiptip_args = {
-		'attribute': 'data-tip',
-		'fadeIn': 50,
-		'fadeOut': 50,
-		'delay': 200
-	};
-	$( '.tips, .help_tip, .restaurantpress-help-tip' ).tipTip( tiptip_args );
+	$( document.body ).on( 'init_tooltips', function() {
+		var tiptip_args = {
+			'attribute': 'data-tip',
+			'fadeIn': 50,
+			'fadeOut': 50,
+			'delay': 200
+		};
 
+		$( '.tips, .help_tip, .restaurantpress-help-tip' ).tipTip( tiptip_args );
+
+		// Add tiptip to parent element for widefat tables
+		$( '.parent-tips' ).each( function() {
+			$( this ).closest( 'a, th' ).attr( 'data-tip', $( this ).data( 'tip' ) ).tipTip( tiptip_args ).css( 'cursor', 'help' );
+		});
+	}).trigger( 'init_tooltips' );
 });
