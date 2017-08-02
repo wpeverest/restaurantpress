24efe18c059366152dc34e671fa9031423286af9
Shiva Poudel <dev.shivapoudel@gmail.com>
2017-08-02 14:31:59 +0545

    uninstall_data replaced with constant.

--- uninstall.php
+++ uninstall.php
@@ -16,9 +16,12 @@ if( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 
 global $wpdb, $wp_version;
 
-$uninstall_data = apply_filters( 'restaurantpress_uninstall_data', true );
-
-if ( $uninstall_data ) {
+/*
+ * Only remove ALL demo importer data if RP_REMOVE_ALL_DATA constant is set to true in user's
+ * wp-config.php. This is to prevent data loss when deleting the plugin from the backend
+ * and to ensure only the site owner can perform this action.
+ */
+if ( defined( 'RP_REMOVE_ALL_DATA' ) && true === RP_REMOVE_ALL_DATA ) {
 	// Roles + caps.
 	include_once( 'includes/class-rp-install.php' );
 	RP_Install::remove_roles();
@@ -36,12 +39,14 @@ if ( $uninstall_data ) {
 	// Delete terms if > WP 4.2 (term splitting was added in 4.2)
 	if ( version_compare( $wp_version, '4.2', '>=' ) ) {
 		// Delete term taxonomies
-		$wpdb->delete(
-			$wpdb->term_taxonomy,
-			array(
-				'taxonomy' => 'food_menu_cat',
-			)
-		);
+		foreach ( array( 'food_menu_cat' ) as $taxonomy ) {
+			$wpdb->delete(
+				$wpdb->term_taxonomy,
+				array(
+					'taxonomy' => $taxonomy,
+				)
+			);
+		}
 
 		// Delete orphan relationships
 		$wpdb->query( "DELETE tr FROM {$wpdb->term_relationships} tr LEFT JOIN {$wpdb->posts} posts ON posts.ID = tr.object_id WHERE posts.ID IS NULL;" );
