9da711af75683bddd1a4f165c416745ee733b9a4
Shiva Poudel <dev.shivapoudel@gmail.com>
2017-08-23 05:14:56 +0545

    Taxonomy template

---
+++ templates/taxonomy-food_menu_cat.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * The Template for displaying foods in a food category. Simply includes the archive template
+ *
+ * This template can be overridden by copying it to yourtheme/restaurantpress/taxonomy-food_menu.php.
+ *
+ * HOWEVER, on occasion RestaurantPress will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
+ *
+ * @see     https://docs.wpeverest.com/docs/restaurantpress/template-structure/
+ * @author  WPEverest
+ * @package RestaurantPress/Templates
+ * @version 1.4.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+rp_get_template( 'archive-food.php' );
