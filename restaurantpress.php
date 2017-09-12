75afe7fc4d7c6dd73cfedbf4ee138adef4ce597e
Rabin Shrestha <sun.ravi90@gmail.com>
2017-09-12 16:52:54 +0545

    Version change

--- restaurantpress.php
+++ restaurantpress.php
@@ -3,7 +3,7 @@
  * Plugin Name: RestaurantPress
  * Plugin URI: https://wpeverest.com/wordpress-plugins/restaurantpress/
  * Description: Allows you to create awesome restaurant menus for restaurants, bars, and cafes in no time. Smartly :)
- * Version: 1.4.0-beta.2
+ * Version: 1.4.0
  * Author: WPEverest
  * Author URI: https://wpeverest.com
  * Requires at least: 4.4
