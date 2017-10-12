f8bee0dc0e5476233c7b755102d92d31c460f49f
Shiva Poudel <dev.shivapoudel@gmail.com>
2017-10-12 12:48:21 +0545

    Do not load our archive template right now

--- includes/class-rp-template-loader.php
+++ includes/class-rp-template-loader.php
@@ -37,7 +37,7 @@ class RP_Template_Loader {
 	 * this to the theme (containing a restaurantpress() inside) this will be used for all.
 	 * restaurantpress templates.
 	 *
-	 * @param mixed $template
+	 * @param  string $template The path of the template to include.
 	 * @return string
 	 */
 	public static function template_loader( $template ) {
@@ -72,16 +72,6 @@ class RP_Template_Loader {
 	private static function get_template_loader_default_file() {
 		if ( is_singular( 'food_menu' ) ) {
 			$default_file = 'single-food_menu.php';
-		} elseif ( is_food_menu_taxonomy() ) {
-			$term = get_queried_object();
-
-			if ( is_tax( 'food_menu_cat' ) ) {
-				$default_file = 'taxonomy-' . $term->taxonomy . '.php';
-			} else {
-				$default_file = 'archive-food.php';
-			}
-		} elseif ( is_post_type_archive( 'food_menu' ) ) {
-			$default_file = 'archive-food.php';
 		} else {
 			$default_file = '';
 		}
