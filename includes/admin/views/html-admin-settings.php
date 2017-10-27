db17e2613529efbc43a68f23c9c04a0aadd497c9
Shiva Poudel <dev.shivapoudel@gmail.com>
2017-10-27 10:48:45 +0545

    Prevent notice on settings page when accessing invalid tab

--- includes/admin/views/html-admin-settings.php
+++ includes/admin/views/html-admin-settings.php
@@ -1,9 +1,19 @@
 <?php
 /**
  * Admin View: Settings
+ *
+ * @package RestaurantPress
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+$tab_exists        = isset( $tabs[ $current_tab ] ) || has_action( 'restaurantpress_sections_' . $current_tab ) || has_action( 'restaurantpress_settings_' . $current_tab );
+$current_tab_label = isset( $tabs[ $current_tab ] ) ? $tabs[ $current_tab ] : '';
+
+if ( ! $tab_exists ) {
+	wp_safe_redirect( admin_url( 'admin.php?page=rp-settings' ) );
 	exit;
 }
 
@@ -12,21 +22,20 @@ if ( ! defined( 'ABSPATH' ) ) {
 	<form method="<?php echo esc_attr( apply_filters( 'restaurantpress_settings_form_method_tab_' . $current_tab, 'post' ) ); ?>" id="mainform" action="" enctype="multipart/form-data">
 		<nav class="nav-tab-wrapper rp-nav-tab-wrapper">
 			<?php
-				foreach ( $tabs as $name => $label ) {
-					echo '<a href="' . admin_url( 'admin.php?page=rp-settings&tab=' . $name ) . '" class="nav-tab ' . ( $current_tab == $name ? 'nav-tab-active' : '' ) . '">' . $label . '</a>';
+				foreach ( $tabs as $slug => $label ) {
+					echo '<a href="' . esc_html( admin_url( 'admin.php?page=rp-settings&tab=' . esc_attr( $slug ) ) ) . '" class="nav-tab ' . ( $current_tab === $slug ? 'nav-tab-active' : '' ) . '">' . esc_html( $label ) . '</a>';
 				}
 
 				do_action( 'restaurantpress_settings_tabs' );
 			?>
 		</nav>
-		<h1 class="screen-reader-text"><?php echo esc_html( $tabs[ $current_tab ] ); ?></h1>
+		<h1 class="screen-reader-text"><?php echo esc_html( $current_tab_label  ); ?></h1>
 		<?php
 			do_action( 'restaurantpress_sections_' . $current_tab );
 
 			self::show_messages();
 
 			do_action( 'restaurantpress_settings_' . $current_tab );
-			do_action( 'restaurantpress_settings_tabs_' . $current_tab ); // @deprecated hook
 		?>
 		<p class="submit">
 			<?php if ( ! isset( $GLOBALS['hide_save_button'] ) ) : ?>
