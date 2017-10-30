f443d5643693f1269e9b9be2406a3f818f8ff4a4
Shiva Poudel <dev.shivapoudel@gmail.com>
2017-10-30 23:06:40 +0545

    Introduced formatting functions for datetime and colors

--- includes/rp-formatting-functions.php
+++ includes/rp-formatting-functions.php
@@ -280,6 +280,245 @@ function rp_time_format() {
 }
 
 /**
+ * Convert mysql datetime to PHP timestamp, forcing UTC. Wrapper for strtotime.
+ *
+ * Based on wcs_strtotime_dark_knight() from WC Subscriptions by Prospress.
+ *
+ * @since  1.5.1
+ *
+ * @param string $time_string
+ * @param int|null $from_timestamp
+ *
+ * @return int
+ */
+function rp_string_to_timestamp( $time_string, $from_timestamp = null ) {
+	$original_timezone = date_default_timezone_get();
+
+	// @codingStandardsIgnoreStart
+	date_default_timezone_set( 'UTC' );
+
+	if ( null === $from_timestamp ) {
+		$next_timestamp = strtotime( $time_string );
+	} else {
+		$next_timestamp = strtotime( $time_string, $from_timestamp );
+	}
+
+	date_default_timezone_set( $original_timezone );
+	// @codingStandardsIgnoreEnd
+
+	return $next_timestamp;
+}
+
+/**
+ * Convert a date string to a RP_DateTime.
+ *
+ * @since  1.5.1
+ * @param  string $time_string
+ * @return RP_DateTime
+ */
+function rp_string_to_datetime( $time_string ) {
+	// Strings are defined in local WP timezone. Convert to UTC.
+	if ( 1 === preg_match( '/^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2})(Z|((-|\+)\d{2}:\d{2}))$/', $time_string, $date_bits ) ) {
+		$offset    = ! empty( $date_bits[7] ) ? iso8601_timezone_to_offset( $date_bits[7] ) : rp_timezone_offset();
+		$timestamp = gmmktime( $date_bits[4], $date_bits[5], $date_bits[6], $date_bits[2], $date_bits[3], $date_bits[1] ) - $offset;
+	} else {
+		$timestamp = rp_string_to_timestamp( get_gmt_from_date( gmdate( 'Y-m-d H:i:s', rp_string_to_timestamp( $time_string ) ) ) );
+	}
+	$datetime  = new RP_DateTime( "@{$timestamp}", new DateTimeZone( 'UTC' ) );
+
+	// Set local timezone or offset.
+	if ( get_option( 'timezone_string' ) ) {
+		$datetime->setTimezone( new DateTimeZone( rp_timezone_string() ) );
+	} else {
+		$datetime->set_utc_offset( rp_timezone_offset() );
+	}
+
+	return $datetime;
+}
+
+/**
+ * RestaurantPress Timezone - helper to retrieve the timezone string for a site until.
+ * a WP core method exists (see https://core.trac.wordpress.org/ticket/24730).
+ *
+ * Adapted from https://secure.php.net/manual/en/function.timezone-name-from-abbr.php#89155.
+ *
+ * @since  1.5.1
+ * @return string PHP timezone string for the site
+ */
+function rp_timezone_string() {
+
+	// if site timezone string exists, return it
+	if ( $timezone = get_option( 'timezone_string' ) ) {
+		return $timezone;
+	}
+
+	// get UTC offset, if it isn't set then return UTC
+	if ( 0 === ( $utc_offset = intval( get_option( 'gmt_offset', 0 ) ) ) ) {
+		return 'UTC';
+	}
+
+	// adjust UTC offset from hours to seconds
+	$utc_offset *= 3600;
+
+	// attempt to guess the timezone string from the UTC offset
+	if ( $timezone = timezone_name_from_abbr( '', $utc_offset ) ) {
+		return $timezone;
+	}
+
+	// last try, guess timezone string manually
+	foreach ( timezone_abbreviations_list() as $abbr ) {
+		foreach ( $abbr as $city ) {
+			if ( (bool) date( 'I' ) === (bool) $city['dst'] && $city['timezone_id'] && intval( $city['offset'] ) === $utc_offset ) {
+				return $city['timezone_id'];
+			}
+		}
+	}
+
+	// fallback to UTC
+	return 'UTC';
+}
+
+/**
+ * Get timezone offset in seconds.
+ *
+ * @since  1.5.0
+ * @return float
+ */
+function rp_timezone_offset() {
+	if ( $timezone = get_option( 'timezone_string' ) ) {
+		$timezone_object = new DateTimeZone( $timezone );
+		return $timezone_object->getOffset( new DateTime( 'now' ) );
+	} else {
+		return floatval( get_option( 'gmt_offset', 0 ) ) * HOUR_IN_SECONDS;
+	}
+}
+
+if ( ! function_exists( 'rp_rgb_from_hex' ) ) {
+
+	/**
+	 * Hex darker/lighter/contrast functions for colors.
+	 *
+	 * @param  mixed $color
+	 * @return array
+	 */
+	function rp_rgb_from_hex( $color ) {
+		$color = str_replace( '#', '', $color );
+		// Convert shorthand colors to full format, e.g. "FFF" -> "FFFFFF"
+		$color = preg_replace( '~^(.)(.)(.)$~', '$1$1$2$2$3$3', $color );
+
+		$rgb      = array();
+		$rgb['R'] = hexdec( $color{0} . $color{1} );
+		$rgb['G'] = hexdec( $color{2} . $color{3} );
+		$rgb['B'] = hexdec( $color{4} . $color{5} );
+
+		return $rgb;
+	}
+}
+
+if ( ! function_exists( 'rp_hex_darker' ) ) {
+
+	/**
+	 * Hex darker/lighter/contrast functions for colors.
+	 *
+	 * @param  mixed $color
+	 * @param  int   $factor (default: 30)
+	 * @return string
+	 */
+	function rp_hex_darker( $color, $factor = 30 ) {
+		$base  = rp_rgb_from_hex( $color );
+		$color = '#';
+
+		foreach ( $base as $k => $v ) {
+			$amount      = $v / 100;
+			$amount      = round( $amount * $factor );
+			$new_decimal = $v - $amount;
+
+			$new_hex_component = dechex( $new_decimal );
+			if ( strlen( $new_hex_component ) < 2 ) {
+				$new_hex_component = "0" . $new_hex_component;
+			}
+			$color .= $new_hex_component;
+		}
+
+		return $color;
+	}
+}
+
+if ( ! function_exists( 'rp_hex_lighter' ) ) {
+
+	/**
+	 * Hex darker/lighter/contrast functions for colors.
+	 *
+	 * @param  mixed $color
+	 * @param  int   $factor (default: 30)
+	 * @return string
+	 */
+	function rp_hex_lighter( $color, $factor = 30 ) {
+		$base  = rp_rgb_from_hex( $color );
+		$color = '#';
+
+		foreach ( $base as $k => $v ) {
+			$amount      = 255 - $v;
+			$amount      = $amount / 100;
+			$amount      = round( $amount * $factor );
+			$new_decimal = $v + $amount;
+
+			$new_hex_component = dechex( $new_decimal );
+			if ( strlen( $new_hex_component ) < 2 ) {
+				$new_hex_component = "0" . $new_hex_component;
+			}
+			$color .= $new_hex_component;
+		}
+
+		return $color;
+	}
+}
+
+if ( ! function_exists( 'rp_light_or_dark' ) ) {
+
+	/**
+	 * Detect if we should use a light or dark color on a background color.
+	 *
+	 * @param  mixed  $color
+	 * @param  string $dark (default: '#000000')
+	 * @param  string $light (default: '#FFFFFF')
+	 * @return string
+	 */
+	function rp_light_or_dark( $color, $dark = '#000000', $light = '#FFFFFF' ) {
+
+		$hex = str_replace( '#', '', $color );
+
+		$c_r = hexdec( substr( $hex, 0, 2 ) );
+		$c_g = hexdec( substr( $hex, 2, 2 ) );
+		$c_b = hexdec( substr( $hex, 4, 2 ) );
+
+		$brightness = ( ( $c_r * 299 ) + ( $c_g * 587 ) + ( $c_b * 114 ) ) / 1000;
+
+		return $brightness > 155 ? $dark : $light;
+	}
+}
+
+if ( ! function_exists( 'rp_format_hex' ) ) {
+
+	/**
+	 * Format string as hex.
+	 *
+	 * @param  string $hex
+	 * @return string
+	 */
+	function rp_format_hex( $hex ) {
+
+		$hex = trim( str_replace( '#', '', $hex ) );
+
+		if ( strlen( $hex ) == 3 ) {
+			$hex = $hex[0] . $hex[0] . $hex[1] . $hex[1] . $hex[2] . $hex[2];
+		}
+
+		return $hex ? '#' . $hex : null;
+	}
+}
+
+/**
  * Format phone numbers.
  *
  * @param  string $phone Phone number.
@@ -382,6 +621,24 @@ add_filter( 'restaurantpress_admin_settings_sanitize_option_restaurantpress_pric
 function rp_format_sale_price( $regular_price, $sale_price ) {
 	$price = '<del>' . ( is_numeric( $regular_price ) ? rp_price( $regular_price ) : $regular_price ) . '</del> <ins>' . ( is_numeric( $sale_price ) ? rp_price( $sale_price ) : $sale_price ) . '</ins>';
 	return apply_filters( 'restaurantpress_format_sale_price', $price, $regular_price, $sale_price );
+}
+
+/**
+ * Format a date for output.
+ *
+ * @since  1.5.1
+ * @param  RP_DateTime $date
+ * @param  string $format Defaults to the rp_date_format function if not set.
+ * @return string
+ */
+function rp_format_datetime( $date, $format = '' ) {
+	if ( ! $format ) {
+		$format = rp_date_format();
+	}
+	if ( ! is_a( $date, 'RP_DateTime' ) ) {
+		return date_i18n( $format );
+	}
+	return $date->date_i18n( $format );
 }
 
 /**
