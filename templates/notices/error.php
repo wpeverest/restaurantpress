dd05bda17f3476a149a66c6856be38ba2a471158
Shiva Poudel <dev.shivapoudel@gmail.com>
2017-12-12 19:01:19 +0545

    a11y

--- templates/notices/error.php
+++ templates/notices/error.php
@@ -25,7 +25,7 @@ if ( ! $messages ) {
 }
 
 ?>
-<ul class="restaurantpress-error">
+<ul class="restaurantpress-error" role="alert">
 	<?php foreach ( $messages as $message ) : ?>
 		<li><?php echo wp_kses_post( $message ); ?></li>
 	<?php endforeach; ?>
