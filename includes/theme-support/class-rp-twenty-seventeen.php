8599be716cdb00f66de907be237ba76f211b3f9f
Shiva Poudel <dev.shivapoudel@gmail.com>
2018-01-11 14:56:39 +0545

    Twentyseventeen support

--- includes/theme-support/class-rp-twenty-seventeen.php
+++ includes/theme-support/class-rp-twenty-seventeen.php
@@ -26,12 +26,33 @@ class RP_Twenty_Seventeen {
 		// Add custom wrappers.
 		add_action( 'restaurantpress_before_main_content', array( __CLASS__, 'output_content_wrapper' ), 10 );
 		add_action( 'restaurantpress_after_main_content', array( __CLASS__, 'output_content_wrapper_end' ), 10 );
+		add_filter( 'restaurantpress_enqueue_styles', array( __CLASS__, 'enqueue_styles' ) );
+		add_filter( 'twentyseventeen_custom_colors_css', array( __CLASS__, 'custom_colors_css' ), 10, 3 );
 
 		// Declare theme support for features.
 		add_theme_support( 'restaurantpress', array(
 			'thumbnail_image_width' => 150,
 			'single_image_width'    => 322,
 		) );
+	}
+
+	/**
+	 * Enqueue CSS for this theme.
+	 *
+	 * @param  array $styles Array of registered styles.
+	 * @return array
+	 */
+	public static function enqueue_styles( $styles ) {
+		unset( $styles['restaurantpress-general'] );
+
+		$styles['restaurantpress-twenty-seventeen'] = array(
+			'src'     => str_replace( array( 'http:', 'https:' ), '', RP()->plugin_url() ) . '/assets/css/twenty-seventeen.css',
+			'deps'    => '',
+			'version' => RP_VERSION,
+			'media'   => 'all',
+		);
+
+		return apply_filters( 'restaurantpress_twenty_seventeen_styles', $styles );
 	}
 
 	/**
@@ -51,6 +72,39 @@ class RP_Twenty_Seventeen {
 		echo '</div>';
 		get_sidebar();
 		echo '</div>';
+	}
+
+
+	/**
+	 * Custom colors.
+	 *
+	 * @param  string $css Styles.
+	 * @param  string $hue Color.
+	 * @param  string $saturation Saturation.
+	 * @return string
+	 */
+	public static function custom_colors_css( $css, $hue, $saturation ) {
+		$css .= '
+			.colors-custom .select2-container--default .select2-selection--single {
+				border-color: hsl( ' . $hue . ', ' . $saturation . ', 73% );
+			}
+			.colors-custom .select2-container--default .select2-selection__rendered {
+				color: hsl( ' . $hue . ', ' . $saturation . ', 40% );
+			}
+			.colors-custom .select2-container--default .select2-selection--single .select2-selection__arrow b {
+				border-color: hsl( ' . $hue . ', ' . $saturation . ', 40% ) transparent transparent transparent;
+			}
+			.colors-custom .select2-container--focus .select2-selection {
+				border-color: #000;
+			}
+			.colors-custom .select2-container--focus .select2-selection--single .select2-selection__arrow b {
+				border-color: #000 transparent transparent transparent;
+			}
+			.colors-custom .select2-container--focus .select2-selection .select2-selection__rendered {
+				color: #000;
+			}
+		';
+		return $css;
 	}
 }
 
